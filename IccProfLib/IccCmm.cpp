66a9d9766f5ef07abf2f012185370778ec7573d6
ChrisCoxArt <ccox@comcast.net>
2026-02-03 14:52:12 -0800

    make sure xform matches src and dest channel counts
    Fixes #577

--- IccProfLib/IccCmm.cpp
+++ IccProfLib/IccCmm.cpp
@@ -7570,6 +7570,20 @@ CIccApplyXform *CIccXformMpe::GetNewApply(icStatusCMM &status)
 void CIccXformMpe::Apply(CIccApplyXform* pApply, icFloatNumber *DstPixel, const icFloatNumber *SrcPixel) const
 {
   const CIccTagMultiProcessElement *pTag = m_pTag;
+  
+  // make sure the channel counts for the transform match up!
+  // or we could cause an access violation
+  // this should be validated at a higher level, but sometimes is not
+  icUInt16Number inputSamples = GetNumSrcSamples();
+  icUInt16Number outputSamples = GetNumDstSamples();
+  icUInt16Number xformInputSamples = pTag->NumInputChannels();
+  icUInt16Number xformOutputSamples = pTag->NumOutputChannels();
+  
+  if (inputSamples != xformInputSamples || outputSamples != xformOutputSamples) {
+    // we can't return an error at this point, so just set output to zero
+    memset( DstPixel, 0, outputSamples*sizeof(icFloatNumber) );
+    return;
+  }
 
   icFloatNumber temp[3];
   if (!m_bInput || m_bPcsAdjustXform) { //PCS comming in?
@@ -7809,6 +7823,8 @@ icStatusCMM CIccApplyCmm::Apply(icFloatNumber *DstPixel, const icFloatNumber *Sr
     i = m_Xforms->begin();
 
     // pLastXform = i->ptr->GetXform();  // set, but only used by unused value below
+    // NOTE - at this point, the xform gets channel counts from the profile in and out colorspaces.
+    // it doesn't read the actual transform data, so we can't make sure the channel counts match
     i->ptr->Apply(DstPixel, SrcPixel);
 
 #ifdef DEBUG_CMM_APPLY
