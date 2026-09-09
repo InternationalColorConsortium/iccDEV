f00a3fad1ffea5d1394acba129ebfd394e277393
colourbill-ctrl <colourbill@gmail.com>
2026-09-09 07:45:21 -0700

    fix(#2483): pin the vcpkg port to a commit that is on master (#2484)

--- .github/ci/quality-assurance/scripts/iccApplyNamedCmm-quick-check.sh
+++ .github/ci/quality-assurance/scripts/iccApplyNamedCmm-quick-check.sh
@@ -16,7 +16,9 @@ qa_init "iccApplyNamedCmm-quick-check"
 BIN="$ICCDEV_TOOLS_DIR/IccApplyNamedCmm/iccApplyNamedCmm"
 FROM_XML="$ICCDEV_TOOLS_DIR/IccFromXml/iccFromXml"
 DATA="$ICCDEV_ROOT/.github/ci/test-data/test-data-rgb-16bit.txt"
+DATA8="$ICCDEV_ROOT/Testing/ApplyDataFiles/rgb8bit.txt"
 FLOAT_DATA="$ICCDEV_ROOT/.github/ci/test-data/test-data-rgb-float.txt"
+CMYK_DATA="$ICCDEV_ROOT/Testing/hybrid/Data/cmykGrays.txt"
 PROFILE="$ICCDEV_ROOT/Testing/sRGB_v4_ICC_preference.icc"
 BRDF_XML="$ICCDEV_ROOT/.github/ci/test-data/dtob-brdf.xml"
 CALC_XML="$ICCDEV_ROOT/Testing/Calc/argbCalc.xml"
@@ -30,8 +32,8 @@ CFG="$QA_OUTDIR/named.json"
 
 qa_require_tool "$BIN"
 qa_require_tool "$FROM_XML"
-for fixture in "$DATA" "$FLOAT_DATA" "$PROFILE" "$BRDF_XML" "$CALC_XML" \
-    "$HYBRID_XML" "$SPECTRAL_XML"; do
+for fixture in "$DATA" "$DATA8" "$FLOAT_DATA" "$CMYK_DATA" "$PROFILE" \
+    "$BRDF_XML" "$CALC_XML" "$HYBRID_XML" "$SPECTRAL_XML"; do
     qa_require_file "$fixture"
 done
 if [ "$QA_FAILURES" -ne 0 ]; then
@@ -44,10 +46,10 @@ qa_run build-calc success "" "$FROM_XML" "$CALC_XML" "$CALC"
 qa_run build-hybrid success "" "$FROM_XML" "$HYBRID_XML" "$HYBRID"
 qa_run build-spectral success "" "$FROM_XML" "$SPECTRAL_XML" "$SPECTRAL"
 
-qa_run basic success "" "$BIN" "$DATA" 0 0 "$PROFILE" 1
+qa_run basic success "" "$BIN" "$DATA8" 0 0 "$PROFILE" 1
 qa_run v5-brdf-direct success "" "$BIN" "$DATA" 6 1 "$BRDF" 10063
 qa_run v5-spectral-chain success "" "$BIN" \
-    "$ICCDEV_ROOT/Testing/hybrid/Data/cmykGrays.txt" 3 1 "$HYBRID" 10103 "$SPECTRAL" 10
+    "$CMYK_DATA" 3 1 "$HYBRID" 10103 "$SPECTRAL" 10
 qa_run debug success "" "$BIN" -debugcalc "$FLOAT_DATA" 3:8:12 0 "$CALC" 0
 qa_run environment success "" "$BIN" "$DATA" 0 0 \
     -ENV:bkgX 0.0985 -ENV:bkgY 0.159 -ENV:bkgZ 0.122 "$PROFILE" 1
